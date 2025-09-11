5b8d06fec95016533efcd1e028d6f88a5905474d
ふかふかのさめ <yumika971@gmail.com>
2025-09-11 23:40:48 +0900

    20250911 初回コミット

---
+++ src/main/java/search/command/create_command/CreateCommandApplication.java
@@ -0,0 +1,13 @@
+package search.command.create_command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateCommandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/search/command/create_command/CreateCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package search.command.create_command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
