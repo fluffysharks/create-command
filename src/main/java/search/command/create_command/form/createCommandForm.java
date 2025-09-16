ac96cc630fa6f56a6ba714d5d516ee90f7e2bdf4
ふかふかのさめ <yumika971@gmail.com>
2025-09-16 23:43:27 +0900

    20250916　条件項目とformクラスを追加

---
+++ src/main/java/search/command/create_command/form/createCommandForm.java
@@ -0,0 +1,25 @@
+package search.command.create_command.form;
+
+public class createCommandForm {
+    /** ユーザー名 */
+    private String userName;
+
+    /**
+     * ユーザー名を取得
+     * 
+     * @return ユーザー名
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * ユーザー名を設定
+     * 
+     * @param userName ユーザー名
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
