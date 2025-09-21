775e8a63bb00f43278ea9be291971a144210c27f
ふかふかのさめ <yumika971@gmail.com>
2025-09-21 23:27:08 +0900

    20250921 検索条件を追加

--- src/main/java/search/command/create_command/Controller/CreateCommandController.java
+++ src/main/java/search/command/create_command/Controller/CreateCommandController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.micrometer.common.util.StringUtils;
 import search.command.create_command.form.createCommandForm;
 
 @Controller
@@ -16,11 +17,47 @@ public class CreateCommandController {
         return "createCommand";
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value = "/createTwitterCommand", params = "generateCommand", method = RequestMethod.POST)
     public String displayCommand(Model model, createCommandForm form) {
         StringBuilder command = new StringBuilder();
-        command.append("form:" + form.getUserName());
+
+        if (!StringUtils.isEmpty(form.getUserName())) {
+            // 「ユーザー名」が入力されていれば、from検索を追加
+            command.append("from:" + form.getUserName() + " ");
+        }
+
+        if (!StringUtils.isEmpty(form.getDateSince())) {
+            // 「期間（Since）」が入力されていればsince検索を追加
+            command.append("since:" + form.getDateSince() + " ");
+        }
+
+        if (!StringUtils.isEmpty(form.getDateUntil())) {
+            // 「期間（To）」が入力されていればto検索を追加
+            command.append("until:" + form.getDateUntil() + " ");
+        }
+
+        if (!StringUtils.isEmpty(form.getMinFaves())) {
+            // 「いいね数（Min）」が入力されていればmin_faves検索を追加
+            command.append("min_faves:" + form.getMinFaves() + " ");
+        }
+
+        if (!StringUtils.isEmpty(form.getMaxFaves())) {
+            // 「いいね数（Max）」が入力されていればmax_faves検索を追加F
+            command.append("max_faves:" + form.getMaxFaves() + " ");
+        }
+
+        if (!StringUtils.isEmpty(form.getDateUntil())) {
+            // 「フォロイーの投稿に絞って表示」が入力されていればfilter:followsを追加
+            command.append("filter:follows ");
+        }
+        // if (!StringUtils.isEmpty(form.getDateUntil())) {
+        // // 「フォロワーの投稿に絞って表示」が入力されていればを追加
+        // command.append("filter:followers ");
+        // }
+
         model.addAttribute("command", command);
+
+        model.addAttribute("form", form);
         return "createCommand";
     }
 
--- src/main/java/search/command/create_command/form/createCommandForm.java
+++ src/main/java/search/command/create_command/form/createCommandForm.java
@@ -3,6 +3,18 @@ package search.command.create_command.form;
 public class createCommandForm {
     /** ユーザー名 */
     private String userName;
+    /** 期間（Since） */
+    private String dateSince;
+    /** 期間（Until） */
+    private String dateUntil;
+    /** フォロイーの投稿を表示 */
+    private String followee;
+    /** フォロワーの投稿を表示 */
+    private String follower;
+    /** いいね数（Min） */
+    private String minFaves;
+    /** いい数（Max） */
+    private String maxFaves;
 
     /**
      * ユーザー名を取得
@@ -22,4 +34,111 @@ public class createCommandForm {
         this.userName = userName;
     }
 
+    /**
+     * 期間（Since）を取得
+     * 
+     * @return 期間（Since）
+     */
+    public String getDateSince() {
+        return dateSince;
+    }
+
+    /**
+     * 期間（Since）を設定
+     * 
+     * @param dateSince 期間（Since）
+     */
+    public void setDateSince(String dateSince) {
+        this.dateSince = dateSince;
+    }
+
+    /**
+     * 期間（Until）を取得
+     * 
+     * @return 期間（Until）
+     */
+    public String getDateUntil() {
+        return dateUntil;
+    }
+
+    /**
+     * 期間（Until）を設定
+     * 
+     * @param dateSince 期間（Until）
+     */
+    public void setDateUntil(String dateUntil) {
+        this.dateUntil = dateUntil;
+    }
+
+    /**
+     * followeeを取得
+     * 
+     * @return followee
+     */
+    public String getFollowee() {
+        return followee;
+    }
+
+    /**
+     * followeeを設定
+     * 
+     * @param followee
+     */
+    public void setFollowee(String followee) {
+        this.followee = followee;
+    }
+
+    /**
+     * followerを取得
+     * 
+     * @return follower
+     */
+    public String getFollower() {
+        return follower;
+    }
+
+    /**
+     * followerを設定
+     * 
+     * @param follower
+     */
+    public void setFollower(String follower) {
+        this.follower = follower;
+    }
+
+    /**
+     * いいね数（Min）を取得
+     * 
+     * @return minFaves
+     */
+    public String getMinFaves() {
+        return minFaves;
+    }
+
+    /**
+     * いいね数（Min）を設定
+     * 
+     * @param minFaves いいね数（Min）
+     */
+    public void setMinFaves(String minFaves) {
+        this.minFaves = minFaves;
+    }
+
+    /**
+     * いいね数（Max）を取得
+     * 
+     * @return maxFaves
+     */
+    public String getMaxFaves() {
+        return maxFaves;
+    }
+
+    /**
+     * いいね数（Max）を設定
+     * 
+     * @param maxFaves いいね数（Max）
+     */
+    public void setMaxFaves(String maxFaves) {
+        this.maxFaves = maxFaves;
+    }
 }
